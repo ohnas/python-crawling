7d15d00b5266072a4efe12e264740b6d988f83ae
ohnas <ohnas12@gmail.com>
2023-07-09 17:50:45 +0900

    modify today_idiom in idioms.py

--- idioms.py
+++ idioms.py
@@ -12,9 +12,9 @@ def today_idiom(headers):
     link = daily.contents[0].find("a")["href"]
     meaning = daily.contents[1].text
     example = daily.contents[2].text.replace("Read more ➺", "").rstrip()
-    daily_tuple = (name, link, meaning, example)
+    daily_list = [name, link, meaning, example]
 
-    return daily_tuple
+    return daily_list
 
 
 def idioms_last_page_numbers(headers):
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import requests
+import sqlite3
 from idioms import today_idiom, all_idioms
 from words import today_word
 
@@ -7,15 +9,41 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
 }
 
+TODAY = datetime.today().strftime("%Y-%m-%d")
 
 try:
     print("crawling...")
-    word = today_word(HEADERS)
+    # word = today_word(HEADERS)
     idiom = today_idiom(HEADERS)
-    idioms = all_idioms(HEADERS)
-    print("Today word :", word)
-    print("Today idiom :", idiom)
-    print("All idioms :", idioms)
+    print(idiom)
+    # con = sqlite3.connect("crawling.db")
+    # cur = con.cursor()
+    # cur.execute(
+    #     """
+    #     CREATE TABLE IF NOT EXISTS daily_idiom
+    #         (id INTEGER PRIMARY KEY, date text, name text, link text, meaning text, example text)
+    # """
+    # )
+    # cur.execute(
+    #     """
+    #     CREATE TABLE IF NOT EXISTS all_idioms
+    #         (id INTEGER PRIMARY KEY, name text, link text, meaning text, example text)
+    # """
+    # )
+    # cur.execute(
+    #     """
+    #     CREATE TABLE IF NOT EXISTS daily_word
+    #         (id INTEGER PRIMARY KEY, date text, word text, img_src text, link text)
+    # """
+    # )
+    # cur.execute('INSERT INTO daily_idiom VALUES (?,?,?,?,?)', idiom)
+
+    # con.commit()
+    # con.close()
+    # idioms = all_idioms(HEADERS)
+    # print("Today word :", word)
+    # print("Today idiom :", idiom)
+    # print("All idioms :", idioms)
     print("finish!")
 except requests.exceptions.HTTPError as err:
     print(f"HTTP error occurred: {err}")
